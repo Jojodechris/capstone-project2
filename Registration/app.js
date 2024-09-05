ef87753f1ef2dfbd7e59a182af21d6a5644d171b
christophe jordan noubissi <christophejordannoubissi@christophes-MacBook-Air.local>
2024-09-04 21:22:31 -0400

    resave true

--- Registration/app.js
+++ Registration/app.js
@@ -52,11 +52,11 @@ app.use(
   session({
     key: "user",
     secret: "secret",
-    resave: false,
+    resave: true,
     // what works loacally
     // saveUninitialized: true,
     // secure:false,
-    saveUninitialized: false,
+    saveUninitialized: true,
     cookie: {
       // secure: false, // Ensure cookies are only sent over HTTPS in production
       // sameSite: strict, // Prevents CSRF attacks; use 'strict' in production
