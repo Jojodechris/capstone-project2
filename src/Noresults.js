0efc842bf385a6c7d023021d6d4a4d3f3f6aeb55
christophe jordan noubissi <christophejordannoubissi@christophes-MacBook-Air.local>
2024-09-17 23:08:24 -0400

    improving signup/login page2

---
+++ src/Noresults.js
@@ -0,0 +1,15 @@
+import React from "react";
+// import "bootstrap-icons/font/bootstrap-icons.css";
+// import NavBar from "./NavBar";
+// import HomeNav from "./HomeNav";
+
+
+
+const Noresults = () => {
+  return (
+    <div className="no-results">
+         
+    </div>
+  );
+};
+export default Noresults;
