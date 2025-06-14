80fc22fac9ecaa8a37384077bd82e279f613c8c2
christophe jordan noubissi <christophejordannoubissi@macbook-pro.mynetworksettings.com>
2025-06-14 09:29:21 -0400

    real_supkeyandurl

--- Registration/supabaseClient.js
+++ Registration/supabaseClient.js
@@ -1,8 +1,8 @@
 const { createClient } = require('@supabase/supabase-js');
 
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im12cnhheGRkdm9rb2JtbWJ6dHNqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjI3OTIyMDcsImV4cCI6MjAzODM2ODIwN30.7I58L6yn8ihme9Zop0cjxFy76QxpPxtr1suxUGtushc"
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJpaGNlZGd5Y2t4eWJ3bWd6eG1rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk4Mjk4MjgsImV4cCI6MjA2NTQwNTgyOH0.hgFAkl_N2b3ez8I0h_0PadeKIweRV5ePHdeOjUssfRs"
 
-const supabaseUrl = 'https://mvrxaxddvokobmmbztsj.supabase.co';
+const supabaseUrl = 'https://rihcedgyckxybwmgzxmk.supabase.co';
 
 // const supabaseKey= 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1ibGJ6Y2h4b3NxeXF3anV0dWVvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjczNjMxOTQsImV4cCI6MjA0MjkzOTE5NH0.Q5s9c45ykJWd4C6Y9GaJHgcq-_tq84UXBZLO2VOe2BI'
 // const supabaseUrl= 'https://mblbzchxosqyqwjutueo.supabase.co'
